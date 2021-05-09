3c28eb8a3f70c8cd0d5b94acbdb263ed84015cbd
Sara Green <smg105c@gmail.com>
2021-05-09 13:12:13 -0700

    added visible/hidden properties to navbar and continued styling

--- src/Home/home.js
+++ src/Home/home.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import './home.css'
+import photo from '../images/mountains-2.jpg'
 
 class Home extends Component {
     render() {
         return (
-            <div className="home_container">
+            <div id="home_container">
                 <div className="home_bubble">
                     <h1>Hi, I'm Sara Green</h1>
                     <h2>A Software Developer in Mammoth Lakes, CA.</h2>
+                    <img className="photo" src={photo} />
                     <a href="#about_container" id="home_btn">Learn More</a>    
                 </div>
                 
--- src/Navbar/navbar.js
+++ src/Navbar/navbar.js
@@ -2,21 +2,59 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import './navbar.css'
 
-
 class Navbar extends Component {
+    constructor() {
+        super();
+        this.state = {
+            show: false,
+            scrollPos: 0
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll)
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleScroll);
+    }
+
+    handleScroll = () => {
+        this.setState({
+            scrollPos: document.body.getBoundingClientRect().top,
+            show: document.body.getBoundingClientRect().top < -760
+        });
+    }
+
     render() {
         return (
-            <div>
+            <nav className={this.state.show ? "active" : "hidden"}>
                 <ul id="nav">
-                    <li><Link to={"/"}>Home</Link></li>
+                    <li><a href="home_container">Home</a></li>
                     <li><a href="#about_container">About</a></li>
                     <li><a href="#projects_container">Projects</a></li>
                     <li><a href="#contact_container">Contact Me</a></li>
-                </ul>
-            </div>
+              </ul>
+            </nav>
         )
     }
 }
+
+
+// class Navbar extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <ul id="nav">
+//                     <li><Link to={"/"}>Home</Link></li>
+//                     <li><a href="#about_container">About</a></li>
+//                     <li><a href="#projects_container">Projects</a></li>
+//                     <li><a href="#contact_container">Contact Me</a></li>
+//                 </ul>
+//             </div>
+//         )
+//     }
+// }
 
 
 export default Navbar
